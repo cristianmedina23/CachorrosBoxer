e10d3fb956931a10a7157c98a11f7f63bb832208
Pc <Pc@DESKTOP-G8FBE6Q>
2024-12-05 17:06:17 -0300

    Agregar archivos de proyecto.

---
+++ CachorrosBoxer.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CachorrosBoxer.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
